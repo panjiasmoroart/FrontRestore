ee3c5658fa4d74d6c8baa6f267d0f5c193a84d67
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-11 22:56:18 +0700

    React Basics - React Component Basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+const products = [
+  { name: "Product 1", price: 10 },
+  { name: "Product 2", price: 20 },
+  { name: "Product 3", price: 30 },
+];
+
 function App() {
   return (
-    <>
-      <div style={{ fontSize: "1.6rem" }}>Front Restore</div>
-    </>
+    <div>
+      <h1>Front Restore</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
