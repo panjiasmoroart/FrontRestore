e7a2535a4586724770f7d8ae920aebbfc1f3f976
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-12 23:22:16 +0700

    React Basics - Adding a Navigation bar into the App

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
+import NavBar from "./NavBar";
 
 // const arrProducts = [
 //   { name: "Product 1", price: 100 },
@@ -18,33 +19,13 @@ function App() {
       .then((data) => setProducts(data));
   }, []);
 
-  const addProduct = () => {
-    // setProducts([...products, { name: "Product 4", price: 40 }]);
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        id: prevState.length + 1,
-        name: "Product " + (prevState.length + 1),
-        description: "Description for product " + (prevState.length + 1),
-        price: (prevState.length + 1) * 100,
-        pictureUrl: "https://via.placeholder.com/150",
-        type: "Type " + (prevState.length + 1),
-        brand: "Brand " + (prevState.length + 1),
-        quantityInStock: 10,
-      },
-    ]);
-  };
-
   return (
-    <Container maxWidth="xl">
-      <Box display="flex" justifyContent="center" gap={3} marginY={4}>
-        <Typography variant="h4">.NET React</Typography>
-        <Button variant="contained" onClick={addProduct}>
-          Add Product
-        </Button>
-      </Box>
-      <Catalog products={products} />
-    </Container>
+    <>
+      <NavBar />
+      <Container maxWidth="xl" sx={{ mt: 14 }}>
+        <Catalog products={products} />
+      </Container>
+    </>
   );
 }
 
