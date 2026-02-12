61eee778e01297ef7f1c8d5d83cf831a58811422
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-12 21:58:46 +0700

    React Basics - Adding a TypeScript type for the Product

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import type { Product } from "./product";
 
 // const arrProducts = [
 //   { name: "Product 1", price: 100 },
@@ -7,9 +8,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   // const [products, setProducts] = useState(arrProducts);
-  const [products, setProducts] = useState<{ name: string; price: number }[]>(
-    [],
-  );
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch("https://localhost:5001/api/products")
@@ -22,8 +21,14 @@ function App() {
     setProducts((prevState) => [
       ...prevState,
       {
+        id: prevState.length + 1,
         name: "Product " + (prevState.length + 1),
+        description: "Description for product " + (prevState.length + 1),
         price: (prevState.length + 1) * 100,
+        pictureUrl: "https://via.placeholder.com/150",
+        type: "Type " + (prevState.length + 1),
+        brand: "Brand " + (prevState.length + 1),
+        quantityInStock: 10,
       },
     ]);
   };
