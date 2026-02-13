fc7a5b22b0fe53bbebff80ceed413d12c9d27a39
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-13 14:07:50 +0700

    React Basics - Styling the product details page part 1

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,6 +1,16 @@
 import { useParams } from "react-router";
 import type { Product } from "../../app/models/product";
 import { useEffect, useState } from "react";
+import {
+  Button,
+  Divider,
+  Grid,
+  Table,
+  TableBody,
+  TableContainer,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 export default function ProductDetails() {
   const { id } = useParams();
@@ -13,5 +23,56 @@ export default function ProductDetails() {
       .catch((error) => console.log(error));
   }, []);
 
-  return <div>{product?.name}</div>;
+  if (!product) return <div>Loading...</div>;
+
+  return (
+    <Grid container spacing={6} maxWidth="lg" sx={{ mx: "auto" }}>
+      <Grid size={6}>
+        <img
+          src={product?.pictureUrl}
+          alt={product?.name}
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <TableContainer>
+          <Table
+            sx={{
+              "& td": { fontSize: "1rem" },
+            }}
+          >
+            <TableBody>Table goes here</TableBody>
+          </Table>
+        </TableContainer>
+        <Grid container spacing={2} marginTop={3}>
+          <Grid size={6}>
+            <TextField
+              variant="outlined"
+              type="number"
+              label="Quantity in basket"
+              fullWidth
+              value={4}
+            />
+          </Grid>
+          <Grid size={6}>
+            <Button
+              disabled={false}
+              sx={{ height: "55px" }}
+              color="primary"
+              size="large"
+              variant="contained"
+              fullWidth
+            >
+              Add to basket
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
