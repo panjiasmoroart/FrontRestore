a404e86c5fc002714df9530b364f28291084f0b1
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-13 23:36:18 +0700

    Redux Toolkit (State Management) - Creating a custom base query for RTK Query

--- src/features/catalog/catalogApi.ts
+++ src/features/catalog/catalogApi.ts
@@ -1,9 +1,10 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import type { Product } from '../../app/models/product';
+import { baseQueryWithErrorHandling } from '../../app/api/baseApi';
 
 export const catalogApi = createApi({
   reducerPath: 'catalogApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:5001/api' }),
+  baseQuery: baseQueryWithErrorHandling,
   endpoints: (builder) => ({
     fetchProducts: builder.query<Product[], void>({
       query: () => ({ url: 'products' }),
