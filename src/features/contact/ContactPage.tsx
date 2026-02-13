1a453fc3ba0fed617cb761b696f69e1ecbeea925
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-13 16:48:18 +0700

    Redux Toolkit (State Management) - Redux Actions

--- src/features/contact/ContactPage.tsx
+++ src/features/contact/ContactPage.tsx
@@ -1,15 +1,28 @@
-import { Typography } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import type { CounterState } from "./counterReducer";
 
 export default function ContactPage() {
   const data = useSelector((state: CounterState) => state.data);
+  const dispatch = useDispatch();
 
   return (
     <>
       <Typography variant="h2">Contact Page</Typography>
 
       <Typography variant="body1">The data is: {data}</Typography>
+
+      <ButtonGroup>
+        <Button color="error" onClick={() => dispatch({ type: "decrement" })}>
+          Decrement
+        </Button>
+        <Button
+          color="secondary"
+          onClick={() => dispatch({ type: "increment" })}
+        >
+          Increment
+        </Button>
+      </ButtonGroup>
     </>
   );
 }
