534be29b646ae3b48172c9e8a22f9b2cae721b0b
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-13 13:37:12 +0700

    React Basics - Fetching an individual product From the API

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,3 +1,17 @@
+import { useParams } from "react-router";
+import type { Product } from "../../app/models/product";
+import { useEffect, useState } from "react";
+
 export default function ProductDetails() {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    fetch(`https://localhost:5001/api/products/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return <div>{product?.name}</div>;
 }
