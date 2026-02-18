a1ed994907f2712113c3f7f96f4d3a4e338a864e
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-18 09:27:42 +0700

    Error Handling - Using Type Guards

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -6,6 +6,8 @@ const customBaseQuery = fetchBaseQuery({
   baseUrl: "https://localhost:5001/api",
 });
 
+type ErrorResponse = | string | { title: string } | { errors: string[] };
+
 const sleep = () => new Promise(resolve => setTimeout(resolve, 1000));
 
 export const baseQueryWithErrorHandling = async (args: string | FetchArgs, api: BaseQueryApi, extraOptions: object) => {
@@ -14,23 +16,43 @@ export const baseQueryWithErrorHandling = async (args: string | FetchArgs, api: 
   const result = await customBaseQuery(args, api, extraOptions);
   api.dispatch(stopLoading());
   if (result.error) {
+    console.log(result.error)
     // const { status, data } = result.error;
     // console.log({ status, data });
     const originalStatus = result.error.status === 'PARSING_ERROR' && result.error.originalStatus
       ? result.error.originalStatus
       : result.error.status
 
-    const responseData = result.error.data;
+    const responseData = result.error.data as ErrorResponse;
 
     switch (originalStatus) {
       case 400:
-        toast.error(responseData as string)
+        if (typeof responseData === 'string') toast.error(responseData);
+        else if ('errors' in responseData) {
+          toast.error('validation error');
+          // throw Object.values(responseData.errors).flat().join(', ')
+        }
+        else toast.error(responseData.title);
         break;
       case 401:
-        toast.error(responseData.title)
+        if (typeof responseData === 'object' && 'title' in responseData)
+          toast.error(responseData.title)
+        break;
+      case 403:
+        if (typeof responseData === 'object')
+          toast.error('403 Forbidden');
+        break;
+      case 404:
+        if (typeof responseData === 'object' && 'title' in responseData)
+          toast.error(responseData.title)
+        break;
+      case 500:
+        if (typeof responseData === 'object' && 'title' in responseData)
+          toast.error(responseData.title)
         break;
       default:
         console.log("Unexpected error status:", originalStatus);
+        break;
     }
   }
 
