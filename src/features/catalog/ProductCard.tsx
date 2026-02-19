a033129f4209b8f1e5c2cc3fefe01d938d10be37
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-19 15:31:44 +0700

    Adding Shopping Cart Feature - Updating the product card to use the add item to cart

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -8,12 +8,15 @@ import {
 } from "@mui/material";
 import type { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
+import { useAddBasketItemMutation } from "../basket/basketApi";
 
 type Props = {
   product: Product;
 };
 
 export default function ProductCard({ product }: Props) {
+  const [addBasketItem, { isLoading }] = useAddBasketItemMutation();
+
   return (
     <Card
       elevation={3}
@@ -43,7 +46,12 @@ export default function ProductCard({ product }: Props) {
         </Typography>
       </CardContent>
       <CardActions sx={{ justifyContent: "space-between" }}>
-        <Button>Add to cart</Button>
+        <Button
+          onClick={() => addBasketItem({ productId: product.id, quantity: 1 })}
+          disabled={isLoading}
+        >
+          Add to cart
+        </Button>
         <Button component={Link} to={`/catalog/${product.id}`}>
           View
         </Button>
