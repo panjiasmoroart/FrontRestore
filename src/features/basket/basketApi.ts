40568dd638bb38d0b8643242ab064ff3e88d0cf1
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-19 10:18:17 +0700

    Adding Shopping Cart Feature - Adding the RTK Query api for the basket/cart

---
+++ src/features/basket/basketApi.ts
@@ -0,0 +1,28 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQueryWithErrorHandling } from "../../app/api/baseApi";
+import type { Basket } from "../../app/models/basket";
+// import type { Product } from "../../app/models/product";
+
+export const basketApi = createApi({
+  reducerPath: "basketApi",
+  baseQuery: baseQueryWithErrorHandling,
+  endpoints: (builder) => ({
+    fetchBasket: builder.query<Basket, void>({
+      query: () => "basket",
+    }),
+    addBasketItem: builder.mutation<Basket, { productId: number, quantity: number }>({
+      query: ({ productId, quantity }) => ({
+        url: `basket?productId=${productId}&quantity=${quantity}`,
+        method: 'POST'
+      })
+    }),
+    removeBasketItem: builder.mutation<void, { productId: number, quantity: number }>({
+      query: ({ productId, quantity }) => ({
+        url: `basket?productId=${productId}&quantity=${quantity}`,
+        method: 'DELETE'
+      })
+    })
+  })
+});
+
+export const { useFetchBasketQuery } = basketApi;
