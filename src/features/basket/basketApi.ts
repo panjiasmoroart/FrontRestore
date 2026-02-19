35e2a16f93b41cbd5340a13b397292db9e9bf67a
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-19 19:38:21 +0700

    Adding Shopping Cart Feature - Invalidating RTK Query cache

--- src/features/basket/basketApi.ts
+++ src/features/basket/basketApi.ts
@@ -6,15 +6,25 @@ import type { Basket } from "../../app/models/basket";
 export const basketApi = createApi({
   reducerPath: "basketApi",
   baseQuery: baseQueryWithErrorHandling,
+  tagTypes: ["Basket"],
   endpoints: (builder) => ({
     fetchBasket: builder.query<Basket, void>({
       query: () => "basket",
+      providesTags: ["Basket"]
     }),
     addBasketItem: builder.mutation<Basket, { productId: number, quantity: number }>({
       query: ({ productId, quantity }) => ({
         url: `basket?productId=${productId}&quantity=${quantity}`,
         method: 'POST'
-      })
+      }),
+      onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
+        try {
+          await queryFulfilled;
+          dispatch(basketApi.util.invalidateTags(['Basket']))
+        } catch (error) {
+          console.log(error);
+        }
+      }
     }),
     removeBasketItem: builder.mutation<void, { productId: number, quantity: number }>({
       query: ({ productId, quantity }) => ({
