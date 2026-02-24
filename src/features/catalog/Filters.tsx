14459a5b4ea50e400a2daedd02aa53853e362a88
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-24 16:58:12 +0700

    Adding Shopping Cart Feature - Designing the filters component

--- src/features/catalog/Filters.tsx
+++ src/features/catalog/Filters.tsx
@@ -1,8 +1,75 @@
+import {
+  Box,
+  FormControl,
+  Paper,
+  TextField,
+  FormControlLabel,
+  Radio,
+  FormGroup,
+  Checkbox,
+} from "@mui/material";
 import { useFetchFiltersQuery } from "./catalogApi";
+
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "priceDesc", label: "Price: High to low" },
+  { value: "price", label: "Price: Low to high" },
+];
 
 export default function Filters() {
   const { data } = useFetchFiltersQuery();
   console.log(data);
 
-  return <div>Filters go here</div>;
+  return (
+    <Box display="flex" flexDirection="column" gap={3}>
+      <Paper>
+        <TextField
+          label="Search products"
+          variant="outlined"
+          fullWidth
+          type="search"
+        />
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormControl>
+          {sortOptions.map(({ value, label }) => (
+            <FormControlLabel
+              key={label}
+              control={<Radio color="secondary" sx={{ py: 0.7 }} />}
+              label={label}
+              value={value}
+            />
+          ))}
+        </FormControl>
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormGroup>
+          {data &&
+            data.brands.map((item) => (
+              <FormControlLabel
+                key={item}
+                control={
+                  <Checkbox color="secondary" sx={{ py: 0.7, fontSize: 40 }} />
+                }
+                label={item}
+              />
+            ))}
+        </FormGroup>
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormGroup>
+          {data &&
+            data.types.map((item) => (
+              <FormControlLabel
+                key={item}
+                control={
+                  <Checkbox color="secondary" sx={{ py: 0.7, fontSize: 40 }} />
+                }
+                label={item}
+              />
+            ))}
+        </FormGroup>
+      </Paper>
+    </Box>
+  );
 }
