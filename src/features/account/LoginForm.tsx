b4a330da6c9bef13c53008ceded979cd63dfffd8
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-02-26 22:13:20 +0700

    Identity - Introducing React Hook Form

--- src/features/account/LoginForm.tsx
+++ src/features/account/LoginForm.tsx
@@ -8,8 +8,20 @@ import {
   Button,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import type { LoginSchema } from "../../lib/schemas/loginSchema";
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>();
+
+  const onSubmit = async (data: LoginSchema) => {
+    console.log(data);
+  };
+
   return (
     <Container component={Paper} maxWidth="sm" sx={{ borderRadius: 3 }}>
       <Box
@@ -22,15 +34,29 @@ export default function LoginForm() {
         <Typography variant="h5">Sign in</Typography>
         <Box
           component="form"
-          // onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onSubmit)}
           width="100%"
           display="flex"
           flexDirection="column"
           gap={3}
           marginY={3}
         >
-          <TextField fullWidth label="Email" autoFocus />
-          <TextField fullWidth label="Password" type="password" />
+          <TextField
+            fullWidth
+            label="Email"
+            autoFocus
+            {...register("email", { required: "Email is required" })}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            type="password"
+            {...register("password", { required: "Password is required" })}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
           <Button variant="contained" type="submit">
             Sign in
           </Button>
