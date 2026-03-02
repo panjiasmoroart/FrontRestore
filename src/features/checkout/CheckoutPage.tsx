8b61a29fbfad0aef3a997e48dc5bbdcb14f18176
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-03-02 16:16:00 +0700

    Taking Payments - Creating a stepper component

--- src/features/checkout/CheckoutPage.tsx
+++ src/features/checkout/CheckoutPage.tsx
@@ -1,10 +1,13 @@
 import { Grid } from "@mui/material";
 import OrderSummary from "../../app/shared/components/OrderSummary";
+import CheckoutStepper from "./CheckoutStepper";
 
 export default function CheckoutPage() {
   return (
     <Grid container spacing={2}>
-      <Grid size={8}>Checkout stepper goes here</Grid>
+      <Grid size={8}>
+        <CheckoutStepper />
+      </Grid>
       <Grid size={4}>
         <OrderSummary />
       </Grid>
