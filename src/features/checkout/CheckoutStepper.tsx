0180d33289f860f4f2ae0871416946b408aa25a9
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-03-09 06:51:12 +0700

    Adding the Order Feature - Adding a Checkout Success Page

--- src/features/checkout/CheckoutStepper.tsx
+++ src/features/checkout/CheckoutStepper.tsx
@@ -32,14 +32,19 @@ import { useBasket } from "../../lib/hooks/useBasket";
 import { currencyFormat } from "../../lib/util";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { useCreateOrderMutation } from "../orders/orderApi";
 
 const steps = ["Address", "Payment", "Review"];
 
 export default function CheckoutStepper() {
   const [activeStep, setActiveStep] = useState(0);
-  const { data: { name, ...restAddress } = {} as Address, isLoading } =
-    useFetchAddressQuery();
-  // const { data, isLoading } = useFetchAddressQuery();
+  const [createOrder] = useCreateOrderMutation();
+  // const { data: { name, ...restAddress } = {} as Address, isLoading } =
+  //   useFetchAddressQuery();
+  // menghindari, installHook.js:1 Error handled by React Router default ErrorBoundary:
+  // TypeError: Cannot destructure property 'name' of '{}' as it is null
+  const { data, isLoading } = useFetchAddressQuery();
+  const { name, ...restAddress } = data ?? ({} as Address);
   const [updateAddress] = useUpdateUserAddressMutation();
   const [saveAddressChecked, setSaveAddressChecked] = useState(false);
   const elements = useElements();
@@ -88,6 +93,9 @@ export default function CheckoutStepper() {
       if (!confirmationToken || !basket?.clientSecret)
         throw new Error("Unable to process payment");
 
+      const orderModel = await createOrderModel();
+      const orderResult = await createOrder(orderModel);
+
       const paymentResult = await stripe?.confirmPayment({
         clientSecret: basket.clientSecret,
         redirect: "if_required",
@@ -97,7 +105,7 @@ export default function CheckoutStepper() {
       });
 
       if (paymentResult?.paymentIntent?.status === "succeeded") {
-        navigate("/checkout/success");
+        navigate("/checkout/success", { state: orderResult });
         clearBasket();
       } else if (paymentResult?.error) {
         throw new Error(paymentResult.error.message);
@@ -112,6 +120,16 @@ export default function CheckoutStepper() {
     } finally {
       setSubmitting(false);
     }
+  };
+
+  const createOrderModel = async () => {
+    const shippingAddress = await getStripeAddress();
+    const paymentSummary = confirmationToken?.payment_method_preview.card;
+
+    if (!shippingAddress || !paymentSummary)
+      throw new Error("Problem creating order");
+
+    return { shippingAddress, paymentSummary };
   };
 
   const getStripeAddress = async () => {
--- src/features/checkout/CheckoutSuccess.tsx
+++ src/features/checkout/CheckoutSuccess.tsx
@@ -1,11 +1,17 @@
 import { Container, Typography } from "@mui/material";
+import { useLocation } from "react-router-dom";
+import type { Order } from "../../app/models/order";
 
 export default function CheckoutSuccess() {
+  const { state } = useLocation();
+  const order = state as Order;
+
   return (
     <Container maxWidth="md">
       <>
         <Typography variant="h4" gutterBottom fontWeight="bold">
-          Thanks for your fake order!
+          Thanks for your fake order! <br />
+          {JSON.stringify(order, null, 2)}
         </Typography>
       </>
     </Container>
